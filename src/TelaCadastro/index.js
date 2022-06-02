4bc9ee001c16e99046bdb20602043aecdd926fd3
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-06-02 17:56:21 -0300

    added :  Api, SignUp screen and styles
    conclusion of SignUp screen and using the api to make a post of the user data

---
+++ src/TelaCadastro/index.js
@@ -0,0 +1,217 @@
+import React, {useState} from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, Alert, Dimensions, } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {Picker} from '@react-native-picker/picker';
+import { StatusBar } from 'expo-status-bar';
+import styles from './styles';
+
+
+// Icons
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
+
+
+
+export default function TelaCadastro({navigation}) {
+
+
+
+    const {width, heigth} = Dimensions.get('screen');
+
+
+
+
+
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [role,setRole] = useState();
+
+
+
+
+    async function registrar(){
+
+        const res = await fetch('http://switch-gym.herokuapp.com/api/users',{
+            method:'POST',
+            mode: 'cors',
+            headers: new Headers({
+                'Content-Type': "application/json;charset=utf-8"
+            }),
+            body:JSON.stringify({
+                "user": {
+                    "name":name,
+                    "email": email,
+                    "password": password,
+                    "role": role
+                }
+            })
+        });
+        if (res.status != 201) {
+            Alert.alert('Erro ao cadastrar', 'Não foi possivel concluir o cadastro, certifique-se de ter prenchido todos os dados corretamente');
+
+
+            return;
+        }
+
+
+
+        navigation.navigate("Login");
+
+
+
+
+
+    }
+
+
+    return (
+
+
+        <SafeAreaView style={{ width:width, height:heigth, flex:1,  }}>
+            <StatusBar hidden/>
+
+            <Image
+                style={styles.backgroundImg}
+                source={require('../../assets/imgCadastro/FitDreams2.png')}
+            />
+
+
+
+
+
+
+            <Text style={styles.title}> Registre-se</Text>
+
+            {/* TextInput nome */}
+            <View style={styles.textInputContainer}>
+                <Ionicons
+                    name="ios-person-outline"
+                    size={24}
+                    color="black"
+                    style={styles.icons}
+                />
+
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Nome Completo...'
+                    onChangeText={text=>setName(text)}
+
+                />
+            </View>
+            {/* TextInput  email*/}
+            <View style={styles.textInputContainer}>
+                <MaterialCommunityIcons
+                    name="email-outline"
+                    size={24}
+                    color="black"
+                    style={styles.icons}
+                />
+
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Seu e-mail...'
+                    onChangeText={text=>setEmail(text)}
+
+                />
+            </View>
+
+
+            {/* TextInput senha */}
+
+            <View style={styles.textInputContainer}>
+
+                <Feather
+                    name="lock"
+                    size={24}
+                    color="black"
+                    style={styles.icons}
+                />
+
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Sua senha...'
+                    secureTextEntry={true}
+                    onChangeText={text=>setPassword(text)}
+
+
+                />
+            </View>
+
+
+
+            {/* DropDownBox  permite selecionar que funcão o usuario irá exercer e com base nisso ter uma tratativa diferente*/}
+
+            <View style={styles.textInputContainer}>
+
+                <Fontisto
+                    name="person"
+                    size={24}
+                    color="black"
+                    style={styles.icons}
+                />
+
+                <Picker
+                    selectedValue={role}
+                    style={styles.textInput}
+                    onValueChange={(itemValue,) => setRole(itemValue)}
+
+                >
+
+                    <Picker.Item
+                        label='Professor'
+                        value="teacher"
+                    />
+
+                    <Picker.Item
+                        label="Cliente"
+                        value="customer"
+
+                    />
+
+
+                </Picker>
+
+            </View>
+
+
+
+            <TouchableOpacity
+                onPress={() => registrar()}
+                style={styles.btnRegistrar}
+            >
+
+                <Text style={styles.titleBtnRegistrar}>
+                    Registrar-se
+                </Text>
+
+            </TouchableOpacity>
+
+            <View style={styles.BtnAjudaContainer}>
+                <Text pre style={styles.titleAjuda}>
+                    Já possui uma conta?
+                </Text>
+
+
+
+
+                <Text
+                    onPress={()=> navigation.navigate("Login")}
+                    style={styles.titleBtnLogin}
+                > Fazer login
+
+                </Text>
+
+
+            </View>
+
+
+
+
+        </SafeAreaView>
+    );
+}
---
+++ src/TelaCadastro/styles.js
@@ -0,0 +1,115 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+
+    backgroundImg:{
+        marginTop:'5%',
+        width:"60%",
+        height:'30%',
+        resizeMode: 'contain',
+        alignSelf:'center',
+
+
+
+
+
+
+    },
+    title:{
+
+        textAlign:'center',
+        fontSize:20,
+        marginBottom:'0.5%',
+        marginTop:'3%',
+
+    },
+    icons:{
+        color:'blue',
+        paddingLeft:'4%',
+        paddingTop:'0.5%',
+        alignSelf:'center'
+
+    },
+    textInputContainer:{
+
+        flexDirection: 'row',
+        alignSelf:'center',
+        backgroundColor: '#fff',
+        width:'80%',
+        borderRadius:20,
+        marginTop:'2.5%',
+        paddingRight:15
+
+
+
+
+
+    },
+
+
+    textInput:{
+        flex:1,
+        borderRadius:20,
+        borderWidth:0,
+        paddingTop: 15,
+        paddingRight: 0,
+        paddingBottom: 10,
+        paddingLeft: 5,
+        marginLeft: 10,
+        backgroundColor: '#fff',
+        color: '#000',
+
+
+
+
+
+    },
+    btnRegistrar:{
+        Color:'#FFF',
+        backgroundColor:'#1317DD',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:'4%',
+        borderRadius:20,
+        width:'50%',
+        aspectRatio: 10/2.5,
+        alignSelf:'center',
+
+    },
+    btnLogin:{
+        width:'100%',
+        paddingHorizontal:20,
+
+
+
+    },
+    BtnAjudaContainer:{
+
+        flexDirection:'row',
+        width:'80%',
+        paddingLeft:75,
+        marginTop:'0.5%',
+
+        alignItems:'center',
+        justifyContent:'center'
+
+
+
+
+
+    },
+    titleBtnRegistrar:{
+        color: '#FFF',
+        textAlign:'center'
+    },
+    titleBtnLogin:{
+        color: '#1317DD',
+        fontSize:10,
+    },
+    titleAjuda:{
+        fontSize:10,
+    },
+
+
+});
+
+export default styles;
