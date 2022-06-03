27b6771be40af9c84ee62fc5dd545bbbd812b1c6
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-06-02 22:47:23 -0300

    fix import

--- src/TelaLogin/index.js
+++ src/TelaLogin/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert, Image, Dimensions} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, TextInput, TouchableOpacity, Alert, Image, Dimensions, SafeAreaView} from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
 import styles from './styles';
 
