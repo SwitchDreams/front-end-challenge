541fe249bc86db3907bc7332f99d34c0e51b0235
Leo Ribas <leonas.ribas@gmail.com>
2022-06-05 17:54:59 -0300

    🚧 Implementa design da tela de Login

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import Logo from '../../../assets/logo.svg'
+import {
+  useTheme,
+  Center,
+  Box,
+  FormControl,
+  Text,
+  Input,
+  Stack,
+  Icon,
+  Button,
+  KeyboardAvoidingView,
+} from 'native-base'
+import BackgroundImage from '../../components/BackgroundImage'
+import { Feather } from '@expo/vector-icons'
+
+type Props = {}
+
+const Login = (props: Props) => {
+  const { colors } = useTheme()
+
+  return (
+    <BackgroundImage screen="Login">
+      <KeyboardAvoidingView h={{ base: '400px' }} flex={1} behavior="height">
+        <Box
+          px={6}
+          pb={8}
+          style={{
+            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Box>
+            <Logo width={130} />
+            <Text
+              fontSize="3xl"
+              color={colors.primary[100]}
+              fontWeight="bold"
+              textAlign="center"
+            >
+              Switch{'\n'}Gym
+            </Text>
+          </Box>
+          <Text color={colors.muted[100]} fontSize="lg" textAlign="center">
+            Dê um switch no seu estilo de vida{'\n'}com a academia que faz
+            acontecer
+          </Text>
+          <Box mb="1/3" width="full">
+            <FormControl>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input
+                size="md"
+                placeholder="Email"
+                InputLeftElement={
+                  <Icon
+                    as={<Feather name="mail" size={20} />}
+                    size={5}
+                    ml={2}
+                    color={colors.primary[400]}
+                  />
+                }
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Senha</FormControl.Label>
+              <Input
+                size="md"
+                placeholder="Senha"
+                InputLeftElement={
+                  <Icon
+                    as={<Feather name="lock" size={20} />}
+                    size={5}
+                    ml={2}
+                    color={colors.primary[400]}
+                  />
+                }
+              />
+            </FormControl>
+          </Box>
+          <Button width="full">Login</Button>
+        </Box>
+      </KeyboardAvoidingView>
+    </BackgroundImage>
+  )
+}
+
+export default Login
