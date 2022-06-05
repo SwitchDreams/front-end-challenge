f7aa445aa95f7d81cfe6a8f091162320eb0ad6a5
Leo Ribas <leonas.ribas@gmail.com>
2022-06-05 17:52:02 -0300

    ✨ Adiciona configuracao de carregamento e rotas

--- App.tsx
+++ App.tsx
@@ -1,10 +1,74 @@
+import * as Font from 'expo-font'
+import { useState, useEffect, useCallback } from 'react'
+import { View } from 'react-native'
+import Login from './src/screens/Login'
 import { StatusBar } from 'expo-status-bar'
-import { NativeBaseProvider, Box } from 'native-base'
+import * as SplashScreen from 'expo-splash-screen'
+import { NativeBaseProvider } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+  Lexend_100Thin,
+  Lexend_200ExtraLight,
+  Lexend_300Light,
+  Lexend_400Regular,
+  Lexend_500Medium,
+  Lexend_700Bold,
+  Lexend_800ExtraBold,
+  Lexend_900Black,
+} from '@expo-google-fonts/lexend'
+import theme from './src/styles/theme'
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+        await Font.loadAsync({
+          Lexend_100Thin,
+          Lexend_200ExtraLight,
+          Lexend_300Light,
+          Lexend_400Regular,
+          Lexend_500Medium,
+          Lexend_700Bold,
+          Lexend_800ExtraBold,
+          Lexend_900Black,
+        })
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
+  }
+
   return (
-    <NativeBaseProvider>
-      <Box>Open up App.tsx to start working on your app!</Box>
-    </NativeBaseProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <NativeBaseProvider theme={theme}>
+        <StatusBar style="light" />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </SafeAreaProvider>
   )
 }
