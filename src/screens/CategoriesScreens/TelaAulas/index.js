329a733c5d834350832e66fffbfddbeb85ce49ed
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-06-06 23:12:18 -0300

    Create TelaAulas

---
+++ src/screens/CategoriesScreens/TelaAulas/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, } from 'react-native';
+
+
+
+export default function  TelaAulas() {
+    return (
+
+        <View>
+            <Text>Aulas</Text>
+        </View>
+
+    );
+}
