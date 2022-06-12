7fbbc7f2f1e0b0c1a2464ac88fedf973954d649a
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-06-12 16:03:22 -0300

    update styles.js

--- src/screens/TelaCadastro/styles.js
+++ src/screens/TelaCadastro/styles.js
@@ -1,3 +1,4 @@
+import { Lato_700Bold } from "@expo-google-fonts/lato";
 import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
 
@@ -7,108 +8,15 @@ const styles = StyleSheet.create({
         height:'30%',
         resizeMode: 'contain',
         alignSelf:'center',
-
-
-
-
-
-
     },
     title:{
-
+        fontFamily: 'Lato_700Bold',
         textAlign:'center',
         fontSize:20,
         marginBottom:'0.5%',
         marginTop:'3%',
 
     },
-    icons:{
-        color:'blue',
-        paddingLeft:'4%',
-        paddingTop:'0.5%',
-        alignSelf:'center'
-
-    },
-    textInputContainer:{
-
-        flexDirection: 'row',
-        alignSelf:'center',
-        backgroundColor: '#fff',
-        width:'80%',
-        borderRadius:20,
-        marginTop:'2.5%',
-        paddingRight:15
-
-
-
-
-
-    },
-
-
-    textInput:{
-        flex:1,
-        borderRadius:20,
-        borderWidth:0,
-        paddingTop: 15,
-        paddingRight: 0,
-        paddingBottom: 10,
-        paddingLeft: 5,
-        marginLeft: 10,
-        backgroundColor: '#fff',
-        color: '#000',
-
-
-
-
-
-    },
-    btnRegistrar:{
-        Color:'#FFF',
-        backgroundColor:'#1317DD',
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop:'4%',
-        borderRadius:20,
-        width:'50%',
-        aspectRatio: 10/2.5,
-        alignSelf:'center',
-
-    },
-    btnLogin:{
-        width:'100%',
-        paddingHorizontal:20,
-
-
-
-    },
-    BtnAjudaContainer:{
-
-        flexDirection:'row',
-        width:'80%',
-        paddingLeft:75,
-        marginTop:'0.5%',
-
-        alignItems:'center',
-        justifyContent:'center'
-
-
-
-
-
-    },
-    titleBtnRegistrar:{
-        color: '#FFF',
-        textAlign:'center'
-    },
-    titleBtnLogin:{
-        color: '#1317DD',
-        fontSize:10,
-    },
-    titleAjuda:{
-        fontSize:10,
-    },
-
 
 });
 
