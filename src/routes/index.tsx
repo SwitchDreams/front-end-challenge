8034fd350ab7c37e360174f53d329b466c0918fc
Keven <kevenluan.sb@hotmail.com>
2023-01-27 08:43:42 -0300

    Arrumei uma rota

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,23 @@
-import {DefaultTheme, NavigationContainer} from "@react-navigation/native";
-import {useAuth} from "../hooks/useAuth";
-import {Loading} from "../components/Loading";
-import {Box} from "native-base";
-import {AppRoutes} from "./app.routes";
-import {AuthRoutes} from "./auth.routes";
-
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { useAuth } from "../hooks/useAuth";
+import { Loading } from "../components/Loading";
+import { Box } from "native-base";
+import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
 const theme = DefaultTheme;
 theme.colors.background = "#ffffff";
 
 export function Routes() {
-
   const { user, isLoadingUserStorageData } = useAuth();
 
-  if(isLoadingUserStorageData) {
-    return <Loading />
+  if (isLoadingUserStorageData) {
+    return <Loading />;
   }
 
   return (
     <Box flex={1} bg="gray.700">
-      <NavigationContainer theme={theme}>
-        {user.email ? <AppRoutes /> : <AuthRoutes />}
-      </NavigationContainer>
+      <NavigationContainer theme={theme}>{user.email ? <AuthRoutes /> : <AppRoutes />}</NavigationContainer>
     </Box>
   );
 }
